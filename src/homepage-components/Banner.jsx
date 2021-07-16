a57f78e285b2ac3eb052bb1986246128a15fbcd1
Phu Trieu <phutrieu95@gmail.com>
2021-07-15 21:39:38 -0700

    Added dropdown menus to 'Home Services' and 'Black Owned' under banner-services

--- src/homepage-components/Banner.jsx
+++ src/homepage-components/Banner.jsx
@@ -19,14 +19,19 @@ const Banner = () => {
           <a href="" className="link-light">
             <span className="banner-info-icon fas fa-bell"></span>
           </a>
-          <a href="">
-            <a className="d-flex link-light">
+          <div className="dropdown">
+            <a className="btn d-flex link-light dropdown-toggle" id="dropdownMenu2" data-bs-toggle="dropdown" aria-expanded="false">
               <span>
                 <img src="./img/user_medium_square.png" alt="" />
               </span>
-              <span className="banner-sort-down-btn fas fa-sort-down" />
+              {/* <span className="banner-sort-down-btn fas fa-sort-down" /> */}
             </a>
-          </a>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenu2">
+              <li><button class="dropdown-item" type="button">Action</button></li>
+              <li><button class="dropdown-item" type="button">Another action</button></li>
+              <li><button class="dropdown-item" type="button">Something else here</button></li>
+            </ul>
+          </div>
         </div>
       </div>
       <div className="banner-form-container text-center">
@@ -46,6 +51,50 @@ const Banner = () => {
             <span className="fas fa-search"/>
           </button>
         </form>
+        <ul className="banner-services">
+          <li>
+            <span className="fas fa-faucet" />
+            <a href="#">Plumbers</a>
+          </li>
+          <li>
+            <span className="fas fa-utensils" />
+            <a href="#">Restaurants</a>
+          </li>
+          <li className="d-inline-block">
+            <span className="fas fa-home" />
+            <div className="banner-services-dropdown dropdown d-inline-block">
+              <a href="#" className="dropdown-toggle" role="button" id="dropdownMenuLink2" data-bs-toggle="dropdown" aria-expanded="false">Home Services</a>
+              <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink2">
+                <li><a class="dropdown-item" href="#">Air Conditioning & Heating</a></li>
+                <li><a class="dropdown-item" href="#">Contractors</a></li>
+                <li><a class="dropdown-item" href="#">Electricians</a></li>
+                <li><a class="dropdown-item" href="#">Home Cleaners</a></li>
+                <li><a class="dropdown-item" href="#">Landscapers</a></li>
+                <li><a class="dropdown-item" href="#">Locksmiths</a></li>
+                <li><a class="dropdown-item" href="#">Movers</a></li>
+                <li><a class="dropdown-item" href="#">Painters</a></li>
+                <li><a class="dropdown-item" href="#">Plumbers</a></li>
+              </ul>
+            </div>
+          </li>
+          <li>
+            <span className="fas fa-motorcycle" />
+            <a href="">Delivery</a>
+          </li>
+          <li>
+            <span className="fas fa-building" />
+            <div className="banner-services-dropdown dropdown d-inline-block">
+              <a href="#" className="dropdown-toggle" role="button" id="dropdownMenuLink3" data-bs-toggle="dropdown" aria-expanded="false">Black Owned</a>
+              <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink3">
+                <li><a class="dropdown-item" href="#">Restaurants</a></li>
+                <li><a class="dropdown-item" href="#">Hair Salons</a></li>
+                <li><a class="dropdown-item" href="#">Nail Salons</a></li>
+                <li><a class="dropdown-item" href="#">Barbershops</a></li>
+                <li><a class="dropdown-item" href="#">Businesses</a></li>
+              </ul>
+            </div>
+          </li>
+        </ul>
       </div>
     </div>
   )
