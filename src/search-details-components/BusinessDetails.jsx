df90a73b6a99115ee2dac9b46d77e92034e31647
Phu Trieu <phutrieu95@gmail.com>
2021-07-23 15:36:29 -0700

    Completed Menu component

--- src/search-details-components/BusinessDetails.jsx
+++ src/search-details-components/BusinessDetails.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import WriteReview from './WriteReview'
+import Menu from './Menu'
 
 const BusinessDetails = () => {
   return (
     <section id="business-details">
       <WriteReview />
+      <Menu />
     </section>
   )
 }
---
+++ src/search-details-components/Menu.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Menu = () => {
+  return (
+    <div className="full-menu">
+      <h5 className="mb-3"><b>Menu</b></h5>
+      <button className="not-write-review-btn">
+        <span className="fas fa-utensils me-2"></span>
+        <b>Full Menu</b>
+      </button>
+    </div>
+  )
+}
+
+export default Menu
