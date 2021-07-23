c50882235902bca5bb47c29ecc2824becdeaa34e
Phu Trieu <phutrieu95@gmail.com>
2021-07-23 14:06:11 -0700

    Completed sticky-menu component

--- src/search-details-components/HugLife.jsx
+++ src/search-details-components/HugLife.jsx
@@ -8,6 +8,7 @@ const HugLife = () => {
       <HugLifeBanner />
       <StickyMenu />
 
+
     </div>
   )
 }
--- src/search-details-components/StickyMenu.jsx
+++ src/search-details-components/StickyMenu.jsx
@@ -26,6 +26,23 @@ const StickyMenu = () => {
           </button>
         </div>
       </div>
+      <div className="bottom-sticky-menu">
+        <div>
+          <a href="" className="text-decoration-none"><b>huglifeicecream.com</b></a>
+          <span className="fas fa-external-link-alt"></span>
+        </div>
+        <div>
+          <b className="mb-0">(714) 760-4345</b>
+          <span className="fas fa-phone-volume"></span>
+        </div>
+        <div>
+          <div>
+            <a href="" className="text-decoration-none"><b>Get Directions</b></a>
+            <div>14241 Euclid St Ste C115 Garden Grove, CA 92843</div>
+          </div>
+          <span className="fas fa-location-arrow"></span>
+        </div>
+      </div>
     </div>
   )
 }
